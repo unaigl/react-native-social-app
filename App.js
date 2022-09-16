852e4ba80aaea802019076ac9b6d24138d4d53c8
unaigl <unaiarki@gmail.com>
2022-09-16 17:09:46 +0200

    setting up

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Main from './src/components/Main.jsx'
+import { NativeRouter } from 'react-router-native'
+
+export default function App () {
+  return <NativeRouter><Main /></NativeRouter>
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import RepositoryList from './RepositoryList.jsx'
+import AppBar from './AppBar.jsx'
+import { Redirect, Route, Switch } from 'react-router-native'
+
+const Main = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Setting up</Text>
+    </View>
+  )
+}
+
+export default Main
