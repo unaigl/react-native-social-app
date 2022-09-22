032f707091cf6eb6f64c3c66b0ca37db40855974
unaigl <unaiarki@gmail.com>
2022-09-22 14:21:54 +0200

    walletConnect conect/disconect wallet + card components

---
+++ README.md
@@ -0,0 +1,13 @@
+# Expo WalletConnect Demo
+
+## What do these changes consist of?
+
+- Polyfilling NodeJS libraries within [metro.config.js](./metro.config.js)
+- Creating and updating [global.ts](./global.ts)
+
+## Run this on your own
+
+- clone repo
+- yarn install
+- expo start
+- scan QR code
---
+++ src/walletConnect/WalletConnectExperience.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
+import Card from "../components/Card";
+
+const shortenAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+};
+
+function Button({ onPress, label, pos }: any) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function WalletConnectExperience() {
+  const connector = useWalletConnect();
+
+  // const [isConnected, setIsConnected] = React.useState(false);
+
+  const connectWallet = React.useCallback(() => {
+    // setIsConnected(true);
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    // setIsConnected(false);
+    return connector.killSession();
+  }, [connector]);
+
+  return (
+    <>
+      {!connector.connected ? (
+        <Button onPress={connectWallet} label="Connect a wallet" />
+      ) : (
+        <>
+          <Text>{shortenAddress(connector.accounts[0])}</Text>
+          <Button pos="center" onPress={killSession} label="Log out" />
+          <Card />
+        </>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#5A45FF",
+    color: "#FFFFFF",
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  text: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
